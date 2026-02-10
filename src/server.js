3dcc00a72555919b09a2e8576cbcecac975227f2
Lucas Gabriel <lucas.gabriel@carta.com>
2026-02-10 18:53:56 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -17,6 +17,8 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 
 server.use(router) //rotas na prota 3000
 
+console.log("Boas-vindas a API do AllBooks")
+
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("API disponível através da url http://localhost:8000")
 })
